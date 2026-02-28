f155cd694fe4e9f0ea773180fa4c81607c5b1035
Jc-965 <jesse58300@gmail.com>
2026-02-27 21:00:01 -0800

    feat: update react bits

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import { motion } from 'framer-motion'
-import ASCIIText from '../../react-bits-upstream/src/ts-default/TextAnimations/ASCIIText/ASCIIText'
+import ASCIIText from './ASCIIText'
 import DecryptedText from './DecryptedText'
 import Magnet from './Magnet'
 
